2400e754de6e1402c03a6af7a8bd3c753d4d24a4
Ted Paulsen <ted-paulsen@uiowa.edu>
2017-12-01 14:14:27 -0600

    created simple websocket controller and lister

---
+++ app/controllers/MySocketController.java
@@ -0,0 +1,14 @@
+package controllers;
+
+import play.mvc.WebSocketController;
+
+public class MySocketController extends WebSocketController {
+
+    /**
+     * this is a test for websockets
+     */
+    public static void socket(String name) {
+        System.out.println("name is:" + name);
+        outbound.send("Hello %s!", name);
+    }
+}
--- app/models/User.java
+++ app/models/User.java
@@ -2,6 +2,12 @@ package models;
 
 import play.db.jpa.Model;
 
+import java.util.List;
+
 public class User extends Model {
-    
+
+    public long phNumber;
+    public String userId;
+    public List<String> chats;
+
 }

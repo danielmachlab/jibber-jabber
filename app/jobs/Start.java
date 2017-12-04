8be67271cc868cffc19977485d06b89ccce9801f
Cooper <cooper-bell@uiowa.edu>
2017-12-03 23:34:55 -0600

    new attempted startup job

--- app/jobs/Start.java
+++ app/jobs/Start.java
@@ -11,12 +11,11 @@ public class Start extends Job {
 
     public void doJob() {
         System.out.println("Job is running");
+
         Chat jibber = new Chat("jibber");
-        jibber.messages.add(new Message("You are now chatting on jibber"));
         jibber.save();
 
         Chat jabber = new Chat("jabber");
-        jabber.messages.add(new Message("You are now chatting on jabber"));
         jabber.save();
     }
 }
--- app/models/Message.java
+++ app/models/Message.java
@@ -3,11 +3,15 @@ package models;
 import play.db.jpa.Model;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Message extends Model {
 
     public String msg;
+
+    @ManyToOne
+    public Chat chat;
 
     public Message(String msg){
         this.msg = msg;
